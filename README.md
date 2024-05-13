4f5be2033518cde6b2a68c83097158f80ddc6f99
Rocky Haque <rockyhaque99@gmail.com>
2024-05-13 15:44:41 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,52 @@
+# Random Password Generator
+
+<img src="https://i.ibb.co/kBJmMQ5/password.png" align="right" width="110" height="110" />
+
+![GitHub repo size](https://img.shields.io/github/repo-size/rockyhaque/secrect-cliper)
+![GitHub stars](https://img.shields.io/github/stars/rockyhaque/secrect-cliper?style=social)
+![GitHub forks](https://img.shields.io/github/forks/rockyhaque/rockyhaque?style=social)
+[![Follow me on LinkedIn](https://img.shields.io/badge/-LinkedIn-blue?style=flat-square&logo=linkedin&logoColor=white&link=https://www.linkedin.com/in/your-linkedin-handle/)](https://www.linkedin.com/in/your-linkedin-handle/)
+[![Follow me on Instagram](https://img.shields.io/badge/-Instagram-E4405F?style=flat-square&logo=instagram&logoColor=white&link=https://www.instagram.com/rocky_haque.10/)](https://www.instagram.com/rocky_haque.10/)
+[![Connect with me on Facebook](https://img.shields.io/badge/-Facebook-1877F2?style=flat-square&logo=facebook&logoColor=white&link=https://www.facebook.com/rockyhaquee/)](https://www.facebook.com/rockyhaquee/)
+
+This is a simple React application for generating random passwords with customizable options. It allows users to specify the length of the password and choose whether to include numbers and special characters.
+
+## Live Link
+
+
+## Features
+
+- Generate random passwords with specified length
+- Customize generated passwords with numbers and special characters
+- Copy generated passwords to clipboard with a single click
+
+## Technologies Used
+
+- React: JavaScript library for building user interfaces
+- Tailwind CSS: A utility-first CSS framework for rapidly building custom designs
+- React Icons: A collection of popular icons for React projects
+- Clipboard API: Interface for copying and pasting data to and from the clipboard
+
+## Getting Started
+
+To run this project locally, follow these steps:
+
+1. Clone this repository to your local machine.
+2. Navigate to the project directory in your terminal.
+3. Install dependencies by running `npm install`.
+4. Start the development server by running `npm run dev`.
+5. Open your web browser and visit `http://localhost:5173` to view the application.
+
+## Usage
+
+1. Adjust the slider to select the desired password length.
+2. Check the "Numbers" checkbox if you want to include numbers in the password.
+3. Check the "Characters" checkbox if you want to include special characters in the password.
+4. Click the copy button next to the generated password to copy it to your clipboard.
+5. Use the copied password wherever you need it.
+
+## Contributing
+
+Contributions are welcome! If you find any issues or have suggestions for improvements, feel free to open an issue or submit a pull request.
+
+
---
+++ src/App.jsx
@@ -0,0 +1,108 @@
+import { useCallback, useEffect, useRef, useState } from "react";
+import { IoCopyOutline } from "react-icons/io5";
+
+function App() {
+  const [length, setLength] = useState(8);
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [charAllowed, setCharAllowed] = useState(false);
+  const [password, setPassword] = useState("");
+
+  const passRef = useRef(null);
+
+  const passGenerator = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+    if (numberAllowed) {
+      str += "0123456789";
+    }
+
+    if (charAllowed) {
+      str += "~!@#$%^&*()-_=+[{]}|;:',<.>?";
+    }
+
+    for (let i = 1; i <= length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1);
+      pass += str.charAt(char);
+    }
+
+    setPassword(pass);
+  }, [length, numberAllowed, charAllowed, setPassword]);
+
+  const copyPassToClipboard = useCallback(() => {
+    passRef.current?.select();
+    passRef.current?.setSelectionRange(0, 32)
+    window.navigator.clipboard.writeText(password);
+  }, [password])
+
+  useEffect(() => {
+    passGenerator();
+  }, [length, numberAllowed, charAllowed, passGenerator]);
+
+  return (
+    <div className="font-raleway">
+      <h1 className="text-3xl font-semibold  text-center text-white mt-32">
+        Generate Your Random Password{" "}
+      </h1>
+
+      <div className="w-full max-w-lg mx-auto shadow-2xl rounded-lg px-8 py-7 my-12 text-sky-600 font-semibold backdrop-blur-sm bg-white/30">
+        <div className="flex shadow-lg rounded-lg overflow-hidden mb-4">
+          <input
+            type="text"
+            value={password}
+            className="outline-none w-full px-5 py-5"
+            placeholder="password"
+            ref={passRef}
+            readOnly
+          />
+          <button onClick={copyPassToClipboard}  className="relative w-16 overflow-hidden bg-sky-300 mr-1 rounded-r-md text-neutral-900 font-semibold shadow-2xl transition-all before:absolute before:bottom-0 before:left-0 before:top-0 before:z-0 before:h-full before:w-0 before:bg-sky-600 before:transition-all before:duration-500 hover:text-white hover:shadow-sky-600 hover:before:left-0 hover:before:w-full ">
+          <span className="relative z-10 left-5">
+            <IoCopyOutline />
+          </span>
+            
+          </button>
+        </div>
+        <div className="flex text-sm gap-x-3">
+          <div className="flex items-center gap-x-1">
+            <input
+              type="range"
+              min={6}
+              max={32}
+              value={length}
+              className="cursor-pointer"
+              onChange={(e) => {
+                setLength(e.target.value);
+              }}
+            />
+            <label className="text-neutral-800">Length: {length}</label>
+          </div>
+          <div className="flex items-center gap-x-1">
+            <input
+              type="checkbox"
+              defaultChecked={numberAllowed}
+              id="numberInput"
+              onChange={() => {
+                setNumberAllowed((prev) => !prev);
+              }}
+            />
+            <label htmlFor="numberInput" className="text-neutral-800">Numbers</label>
+            <div className="flex items-center gap-x-1">
+              <input
+                type="checkbox"
+                defaultChecked={charAllowed}
+                id="characterInput"
+                onChange={() => {
+                  setCharAllowed((prev) => !prev);
+                }}
+              />
+              <label htmlFor="characterInput" className="text-neutral-800">Characters</label>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
